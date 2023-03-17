14f4477a1ff82e0e5b591b38df1ff7f914c9d32e
AlexandrStopkin <stnalex1@gmail.com>
2023-03-17 15:11:21 +0100

    Add Task2 fields and constructor

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -8,5 +8,11 @@
 //  если сами прямоугольники как геометрические фигуры равны - если их можно совместить,
 //  повернув и наложив друг на друга.
 public class Rectangle {
+  private int width;
+  private int length;
 
+  public Rectangle(int width, int length) {
+    this.width = width;
+    this.length = length;
+  }
 }
